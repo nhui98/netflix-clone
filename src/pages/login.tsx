cd3a4cdbe278ed798146a37f9213152f605cd6d5
Nathan <hui.n1998@gmail.com>
2022-09-12 20:22:43 +0100

    login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,114 @@
+import Spinner from "@components/common/Spinner/Spinner";
+import { useAuth } from "@hooks/useAuth";
+import { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
+
+const Login: NextPage = () => {
+  const [login, setLogin] = useState(false);
+  const { signIn, signUp, loading } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+      await signIn(email, password);
+    } else {
+      await signUp(email, password);
+    }
+  };
+
+  if (loading) return <Spinner />;
+
+  return (
+    <div className="relative flex h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Head>
+        <title>Netflix | Login</title>
+        <meta name="description" content="Netflix login page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src={`/login-bg.jpg`}
+        layout="fill"
+        alt="login"
+        objectFit="cover"
+        className="-z-10 hidden opacity-60 sm:inline"
+      />
+
+      <img
+        src="/netflix-logo.svg"
+        width={150}
+        height={150}
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        alt="logo"
+      />
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
+        <h1 className="text-4xl font-semibold">Sign In</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="email"
+              className="input"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
+          </label>
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              placeholder="password"
+              className="input"
+              {...register("password", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
+          </label>
+        </div>
+
+        <button
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}
+        >
+          Sign In
+        </button>
+
+        <div className="text-[gray]">
+          New to Netflix?{" "}
+          <button
+            type="submit"
+            className="text-white hover:underline"
+            onClick={() => setLogin(false)}
+          >
+            Sign up now
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
