690affe1939d1568eabe60bc8a706087d9555c5c
Nathan <hui.n1998@gmail.com>
2022-09-13 13:26:16 +0100

    get single movie util fn

---
+++ src/utils/api/getMovie.ts
@@ -0,0 +1,17 @@
+import { Movie } from "./getMovies";
+
+export const getMovie = async (movie: Movie) => {
+  try {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/${
+        movie?.media_type === "tv" ? "tv" : "movie"
+      }/${movie?.id}?api_key=${
+        process.env.NEXT_PUBLIC_TMBD_API_KEY
+      }&language=en-US&append_to_response=videos`
+    );
+
+    return data.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
