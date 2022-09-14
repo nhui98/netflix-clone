37786bab35869120d7148485d896a075492e23f1
Nathan <hui.n1998@gmail.com>
2022-09-14 17:36:59 +0100

    add firestore stripe payment package to project

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+//  eslint-disable-next-line
+const withTM = require("next-transpile-modules")([
+  "@stripe/firestore-stripe-payments",
+]);
+
+const nextConfig = withTM({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["image.tmdb.org"],
   },
-};
+});
 
 module.exports = nextConfig;
